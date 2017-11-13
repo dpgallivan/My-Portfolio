0f74a9182d4327c6434d072bff7a12a5bd534866
Daniel Gallivan <Daniel Gallivan>
2017-11-13 12:45:31 -0800

    second commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// $(document).ready(function() {
+	
+// 	var options =[
+// 		{selector: '#projects', offset: 400, callback: function(el){
+// 			Materialize.showStaggeredList($(el));
+// 		}}	
+// 	];
+// 	Materialize.fadeInImages($(el));
+// });
